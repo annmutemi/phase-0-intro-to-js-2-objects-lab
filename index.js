1195463f655cb5e85f411a110c1940e19b1c75b6
annmutemi <annshiru92@gmail.com>
2024-07-09 14:51:50 +0300

    Objects modification

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = { name: "ann", streetAddress: "syokimau" }
+ 
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    console.log(employee)
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    console.log(newEmployee)
+    return newEmployee
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+ 
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    console.log(newEmployee)
+    return newEmployee
+}
+ 
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
